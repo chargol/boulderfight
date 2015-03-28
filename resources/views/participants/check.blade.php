b7ca3ec07234cdd40acdb91e03d1ad7862ff92d9
Patrick Kernke <patrick@kernke.net>
2015-03-28 14:48:11 +0100

    spelling error

--- resources/views/participants/check.blade.php
+++ resources/views/participants/check.blade.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 text-center">
-				<h2 class="group-title">Vorhandenen Datensatz ist bereits vorhanden. <br> Punkte überschreiben?</h2>
+				<h2 class="group-title">Datensatz ist bereits vorhanden. <br> Punkte überschreiben?</h2>
 			</div>	
 		</div>
 
